efa55c76a397ab9d7acd2eb6f9c48ede2eb9b56e
@isaac3597 <isaacchebii3597@gmail.com>
2023-03-15 10:46:43 +0300

    Remove Password enter the password . Table case

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'admin');
-define('DB_NAME', 'table');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'Table'); 
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
